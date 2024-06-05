7320c07b12240db598c17b8469d54c455f2caaa5
TechTraversers <traversertech@gmail.com>
2024-06-04 19:30:45 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+var buttonAU = document.getElementById("aboutPage")
+buttonAU.addEventListener("click", function(){
+window.location.href = window.location.href + "aboutus"
+})
+
+var buttonOS = document.getElementById("ourServices")
+buttonOS.addEventListener("click", function(){
+  window.location.href = window.location.href + "ourservices"
+})
+
+var buttonOV = document.getElementById("ourValues")
+buttonOV.addEventListener("click", function(){
+  window.location.href = window.location.href + "ourvalues"
+})

81d23ce80d6efd834ecd1fc5a9468cfa247af901
Codesmith95 <gameknight3301@gmail.com>
2024-11-11 17:12:12 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-var buttonAU = document.getElementById("aboutPage")
-buttonAU.addEventListener("click", function(){
-window.location.href = window.location.href + "aboutus"
-})
-
-var buttonOS = document.getElementById("ourServices")
-buttonOS.addEventListener("click", function(){
-  window.location.href = window.location.href + "ourservices"
-})
-
-var buttonOV = document.getElementById("ourValues")
-buttonOV.addEventListener("click", function(){
-  window.location.href = window.location.href + "ourvalues"
-})
+// Nothing to see here:3

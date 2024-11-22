0490851c4eacc9903e7e191d39931af7cc4f7ca1
Codesmith95 <gameknight3301@gmail.com>
2024-11-22 16:56:29 -0700

    Update script.js

--- downloads/script.js
+++ downloads/script.js
@@ -4,26 +4,25 @@ rpsButton.addEventListener("click", function(){
 })
 
 function download(file, text) {
+  // creating an invisible element
+  let element = document.createElement('a');
+  element.setAttribute('href',
+      'data:text/plain;charset=utf-8, '
+      + encodeURIComponent(text));
+  element.setAttribute('download', file);
+  document.body.appendChild(element);
+  element.click();
 
-            // creating an invisible element
-            let element = document.createElement('a');
-            element.setAttribute('href',
-                'data:text/plain;charset=utf-8, '
-                + encodeURIComponent(text));
-            element.setAttribute('download', file);
-            document.body.appendChild(element);
-            element.click();
+  document.body.removeChild(element);
+}
 
-            document.body.removeChild(element);
-        }
+// Start file download.
 
-        // Start file download.
+document.getElementById("rpsLinux")
+    .addEventListener("click", function () {
+        let text =
+            document.getElementById("text").value;
+        let filename = "rps-linux";
 
-        document.getElementById("rpsLinux")
-            .addEventListener("click", function () {
-                let text =
-                    document.getElementById("text").value;
-                let filename = "rps-linux";
-
-                download(filename, text);
-            }, false);
+        download(filename, text);
+    }, false);
